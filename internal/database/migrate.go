3ab826579afc54268e8c2f80121b1fcde59569fd
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-03-07 15:26:14 +0300

    refactor(migrate): update imports

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -1,9 +1,13 @@
 package database
 
 import (
+	"life-tracker-app-backend/internal/lifetracker/habits"
+	"life-tracker-app-backend/internal/lifetracker/moods"
+	"life-tracker-app-backend/internal/lifetracker/tasks"
+
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) error {
-	return db.AutoMigrate(&Task{}, &Habit{}, &Mood{})
+	return db.AutoMigrate(&tasks.Task{}, &habits.Habit{}, &moods.Mood{})
 }
